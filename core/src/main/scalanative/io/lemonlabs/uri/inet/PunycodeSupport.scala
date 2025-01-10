68f2ca33789244c117ac0c428b1ff30b0fa05ab6
Ondra Pelech <ondra.pelech@gmail.com>
2025-01-10 03:30:28 +0100

    Handle errors

--- core/src/main/scalanative/io/lemonlabs/uri/inet/PunycodeSupport.scala
+++ core/src/main/scalanative/io/lemonlabs/uri/inet/PunycodeSupport.scala
@@ -6,12 +6,19 @@ import scala.scalanative._
 import scala.scalanative.unsafe._
 
 trait PunycodeSupport {
-  def toPunycode(host: String): String = Zone.acquire { implicit z =>
-    val output: Ptr[CString] = stackalloc[CString]()
-    val error = idn2.idn2_to_ascii_8z(input = toCString(host), output = output, flags = IDN2_TRANSITIONAL)
-    val result = fromCString(!output)
-    libc.stdlib.free(!output)
-    result
+  def toPunycode(host: String): String = {
+    val (result, error) = Zone.acquire { implicit z =>
+      val output: Ptr[CString] = stackalloc[CString]()
+      val error = idn2.idn2_to_ascii_8z(input = toCString(host), output = output, flags = IDN2_TRANSITIONAL)
+      val result = fromCString(!output)
+      libc.stdlib.free(!output)
+      (result, error)
+    }
+    if (error < 0) {
+      throw new RuntimeException(s"idn2_to_ascii_8z returned error $error.")
+    } else {
+      result
+    }
   }
 }
 
