c721f3867de2d56b8d2824a5eb383d038c45b2ac
Ondra Pelech <ondra.pelech@gmail.com>
2025-01-10 03:25:29 +0100

    Free memory allocated by idn2_to_ascii_8z

--- core/src/main/scalanative/io/lemonlabs/uri/inet/PunycodeSupport.scala
+++ core/src/main/scalanative/io/lemonlabs/uri/inet/PunycodeSupport.scala
@@ -2,13 +2,16 @@ package io.lemonlabs.uri.inet
 
 import io.lemonlabs.uri.inet.PunycodeSupport._
 
+import scala.scalanative._
 import scala.scalanative.unsafe._
 
 trait PunycodeSupport {
   def toPunycode(host: String): String = Zone.acquire { implicit z =>
-    val result: Ptr[CString] = stackalloc[CString]()
-    val error = idn2.idn2_to_ascii_8z(toCString(host), result, IDN2_TRANSITIONAL)
-    fromCString(!result)
+    val output: Ptr[CString] = stackalloc[CString]()
+    val error = idn2.idn2_to_ascii_8z(input = toCString(host), output = output, flags = IDN2_TRANSITIONAL)
+    val result = fromCString(!output)
+    libc.stdlib.free(!output)
+    result
   }
 }
 
